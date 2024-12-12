30b1ea55db3dcbab53b8241d8d0cec9657525867
Marat Khabibullin <140619138+HabibullinMarat@users.noreply.github.com>
2024-12-12 15:19:57 +0500

    Task4

--- Tyuiu.KhabibullinMR.Sprint6.Task4.V1.Lib/DataService.cs
+++ Tyuiu.KhabibullinMR.Sprint6.Task4.V1.Lib/DataService.cs
@@ -10,10 +10,17 @@ namespace Tyuiu.KhabibullinMR.Sprint6.Task4.V1.Lib
             int len = (stopValue -  startValue) + 1;
             int count = 0;
             double[] result = new double[len];
-            for (int x = startValue; x < stopValue; x++)
+            for (int x = startValue; x < stopValue+1; x++)
             {
-                result[count] = Math.Round((Math.Sin(x)/(x+1.2)) + Math.Cos(x)*7*x - 2, 2);
-                count++;
+                if (x + 1.2 != 0)
+                {
+                    result[count] = Math.Round((Math.Sin(x) / (x + 1.2)) + Math.Cos(x) * 7 * x - 2, 2);
+                    count++;
+                }
+                else
+                {
+                    result[count] = 0;
+                }
             }
             return result;
         }
